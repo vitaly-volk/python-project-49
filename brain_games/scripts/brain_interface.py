014e099051d071cae73217b663e864cd40834a8b
vitaly volk <podcasts.prevail@gmail.com>
2024-04-19 21:22:33 +0200

    change greeting

--- brain_games/scripts/brain_interface.py
+++ brain_games/scripts/brain_interface.py
@@ -1,28 +1,37 @@
+#!/usr/bin/env python3
+
+
 def get_user_name():
     return input("May I have your name? ")
+
     
 def greet_user(user_name):
-    print(f"hello, {user_name}")
+    print(f"Hello, {user_name}!")
+
 
 def ask_question(QUESTION, random_input):
     print(QUESTION)
     print(f"Question: {random_input}")
 
+
 def get_answer():
     return input("Your answer: ")
+
 
 def inform_about_wrong_answer(given_answer, correct_answer, user_name):
     print(f"{given_answer} is wrong answer ;(. Correct answer was {correct_answer}")
     print(f"Let's try again, {user_name}!")
 
+
 def inform_about_right_answer(given_answer, correct_answer, user_name):
     print("Correct!")
+
 
 def inform_about_win(user_name):
     print(f"Congratulations, {user_name}!")
 
-def run_the_game(ATTEMPTS_NUMBER, QUESTION, generate_question):
 
+def run_the_game(ATTEMPTS_NUMBER, QUESTION, generate_question):
     user_is_correct = True
 
     attempts = ATTEMPTS_NUMBER
