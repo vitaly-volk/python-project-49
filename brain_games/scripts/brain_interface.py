2c7495808c0bb9babc7300130e1697ea26e99033
vitaly volk <podcasts.prevail@gmail.com>
2024-04-19 21:54:41 +0200

    change brain_interface after flake8

--- brain_games/scripts/brain_interface.py
+++ brain_games/scripts/brain_interface.py
@@ -4,7 +4,7 @@
 def get_user_name():
     return input("May I have your name? ")
 
-    
+
 def greet_user(user_name):
     print(f"Hello, {user_name}!")
 
@@ -18,12 +18,12 @@ def get_answer():
     return input("Your answer: ")
 
 
-def inform_about_wrong_answer(given_answer, correct_answer, user_name):
-    print(f"{given_answer} is wrong answer ;(. Correct answer was {correct_answer}")
+def inform_wrong_answer(answer, correct_answer, user_name):
+    print(f"{answer} is wrong answer ;(. Correct answer was {correct_answer}")
     print(f"Let's try again, {user_name}!")
 
 
-def inform_about_right_answer(given_answer, correct_answer, user_name):
+def inform_right_answer(answer, correct_answer, user_name):
     print("Correct!")
 
 
@@ -40,22 +40,21 @@ def run_the_game(ATTEMPTS_NUMBER, QUESTION, generate_question):
 
     greet_user(user_name)
 
-    while user_is_correct != False and attempts>0:
+    while user_is_correct and attempts > 0:
 
         random_input, correct_answer = generate_question()
 
         ask_question(QUESTION, random_input)
-        given_answer = get_answer()
+        answer = get_answer()
 
-        if given_answer != correct_answer:
-                user_is_correct = False
+        if answer != correct_answer:
+            user_is_correct = False
 
-        if user_is_correct == False:
-            inform_about_wrong_answer(given_answer, correct_answer, user_name)
+        if not user_is_correct:
+            inform_wrong_answer(answer, correct_answer, user_name)
         else:
-            inform_about_right_answer(given_answer, correct_answer, user_name)
-            attempts-=1
+            inform_right_answer(answer, correct_answer, user_name)
+            attempts -= 1
 
         if attempts == 0:
             inform_about_win(user_name)
-
