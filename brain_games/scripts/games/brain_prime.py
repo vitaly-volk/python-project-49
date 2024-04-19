ca826febe750cc1a45a658b98c9b8e0557414af8
vitaly volk <podcasts.prevail@gmail.com>
2024-04-19 21:31:46 +0200

    correct bug in brain_prime

--- brain_games/scripts/games/brain_prime.py
+++ brain_games/scripts/games/brain_prime.py
@@ -15,7 +15,7 @@ def isPrime(number):
     if number == 1:
         return False
     i = 2
-    while i*i < number:
+    while i*i <= number:
         if number % i == 0:
             return False
         i += 1
