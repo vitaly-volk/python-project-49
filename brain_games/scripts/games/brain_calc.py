ce81c4fa5aab53dd540faea8264747287e70f666
vitaly volk <podcasts.prevail@gmail.com>
2024-04-19 21:26:13 +0200

    change question form for brain_calc

--- brain_games/scripts/games/brain_calc.py
+++ brain_games/scripts/games/brain_calc.py
@@ -13,7 +13,7 @@ def generate_question():
     random_sign = choice(["+","-","*"])
     random_number1 = randint(0,100)
     random_number2 = randint(0,100)
-    random_input = f"{random_number1}{random_sign}{random_number2}"
+    random_input = f"{random_number1} {random_sign} {random_number2}"
     if random_sign == "+":
         correct_answer = str(random_number1+random_number2)
     elif random_sign == "-":
