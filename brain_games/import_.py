7592197be2c19afc4aaf5ff84dd72d5acdbdd0a1
vitaly volk <podcasts.prevail@gmail.com>
2024-04-28 13:45:39 +0300

    remove init from game file names

--- brain_games/import_.py
+++ brain_games/import_.py
@@ -1,8 +1,8 @@
-from .games.brain_calc_init import generate_question as generate_question_calc
-from .games.brain_even_init import generate_question as generate_question_even
-from .games.brain_gcd_init import generate_question as generate_question_gcd
-from .games.brain_prime_init import generate_question as generate_question_prime
-from .games.brain_prog_init import generate_question as generate_question_prog
+from .games.brain_calc import generate_question as generate_question_calc
+from .games.brain_even import generate_question as generate_question_even
+from .games.brain_gcd import generate_question as generate_question_gcd
+from .games.brain_prime import generate_question as generate_question_prime
+from .games.brain_prog import generate_question as generate_question_prog
 
 generate_question_calc
 generate_question_even
