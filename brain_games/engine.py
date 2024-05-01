c2ede0f19c7f85ae1631050a9497fa4cb5fa56de
vitaly volk <podcasts.prevail@gmail.com>
2024-05-01 14:36:30 +0300

    simplify engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,61 +4,35 @@
 ATTEMPTS_NUMBER = 3
 
 
-def get_user_name():
-    return input('May I have your name? ')
-
-
-def greet_user(user_name):
-    print(f'Hello, {user_name}!')
-
-
-def ask_question(QUESTION, random_input):
-    print(QUESTION)
-    print(f'Question: {random_input}')
-
-
-def get_answer():
-    return input('Your answer: ')
-
-
-def inform_wrong_answer(answer, correct_answer, user_name):
-    print(f'{answer} is wrong answer ;(. Correct answer was {correct_answer}')
-    print(f'Let\'s try again, {user_name}!')
-
-
-def inform_right_answer(answer, correct_answer, user_name):
-    print('Correct!')
-
-
-def inform_about_win(user_name):
-    print(f'Congratulations, {user_name}!')
-
-
 def run_game(module):
 
     user_is_correct = True
 
     attempts = ATTEMPTS_NUMBER
 
-    user_name = get_user_name()
+    user_name = input('May I have your name? ')
 
-    greet_user(user_name)
+    print(f'Hello, {user_name}!')
 
     while user_is_correct and attempts > 0:
 
         random_input, correct_answer = module.generate_question()
 
-        ask_question(module.QUESTION, random_input)
-        answer = get_answer()
+        print(module.QUESTION)
+
+        print(f'Question: {random_input}')
+
+        answer = input('Your answer: ')
 
         if answer != correct_answer:
             user_is_correct = False
 
         if not user_is_correct:
-            inform_wrong_answer(answer, correct_answer, user_name)
+            print(f'{answer} is wrong answer ;(. Correct answer was {correct_answer}')
+            print(f'Let\'s try again, {user_name}!')
         else:
-            inform_right_answer(answer, correct_answer, user_name)
+            print('Correct!')
             attempts -= 1
 
         if attempts == 0:
-            inform_about_win(user_name)
+            print(f'Congratulations, {user_name}!')
