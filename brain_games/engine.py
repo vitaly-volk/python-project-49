4d7d466370579db939eec621f86c099e100dadcb
vitaly volk <podcasts.prevail@gmail.com>
2024-05-04 16:21:11 +0300

    refactor

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -6,15 +6,11 @@ ATTEMPTS_NUMBER = 3
 
 def run_game(module):
 
-    user_is_correct = True
-
-    attempts = ATTEMPTS_NUMBER
-
     user_name = input('May I have your name? ')
 
     print(f'Hello, {user_name}!')
 
-    while user_is_correct and attempts > 0:
+    for attempts in range(0, ATTEMPTS_NUMBER):
 
         random_input, correct = module.generate_question()
 
@@ -25,14 +21,10 @@ def run_game(module):
         answer = input('Your answer: ')
 
         if answer != correct:
-            user_is_correct = False
-
-        if not user_is_correct:
             print(f'{answer} is wrong answer ;(. Correct answer was {correct}')
             print(f'Let\'s try again, {user_name}!')
+            return
         else:
             print('Correct!')
-            attempts -= 1
 
-        if attempts == 0:
-            print(f'Congratulations, {user_name}!')
+    print(f'Congratulations, {user_name}!')
--- brain_games/games/brain_prog.py
+++ brain_games/games/brain_prog.py
@@ -5,18 +5,16 @@ NUMBER_START_RAND_FROM = 0
 NUMBER_END_RAND_WITH = 100
 LENGTH_START_RAND_FROM = 6
 LENGTH_END_RAND_WITH = 10
-STEP_START_RAND_FROM = 0
+STEP_START_RAND_FROM = 1
 STEP_END_RAND_WITH = 10
 QUESTION = 'What number is missing in the progression?'
 
 
 def generate_progression(begin, length, step):
-    list_ = []
-    i = 0
-    while i < length:
-        list_.append(begin + i * step)
-        i += 1
-    return list_
+    progression = []
+    for i in range(0, length):
+        progression.append(str(begin + i * step))
+    return progression
 
 
 def generate_question():
@@ -24,14 +22,8 @@ def generate_question():
     length = randint(LENGTH_START_RAND_FROM, LENGTH_END_RAND_WITH)
     step = randint(STEP_START_RAND_FROM, STEP_END_RAND_WITH)
     missing_point = randint(0, length - 1)
-    random_input = ''
-    i = 0
     progression = generate_progression(begin, length, step)
-    while i < length:
-        if i == missing_point:
-            random_input += '.. '
-        else:
-            random_input += str(progression[i]) + ' '
-        i += 1
+    progression[missing_point] = '..'
+    random_input = ' '.join(progression)
     correct_answer = str(begin + (missing_point * step))
     return (random_input, correct_answer)
